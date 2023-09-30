7346c6b93e8a065044b64589e8e39721c847ec6d
Meripip <meripip100@gmail.com>
2023-09-30 07:21:58 -0300

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SofTk_TechOil_Front.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
